0f250768b9e70fb523f47c204f4e6a7d6f2b5b45
Tan Ahmed <tannerahmed@gmail.com>
2025-01-18 19:24:40 +0000

    Add SelectLanguage component for language and translation selection

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,13 +1,5 @@
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { PAGE_LAYOUT_HEIGHT } from "@/consts/constants";
+import SelectLanguage from "./_components/select-language";
 
 export default function Page() {
   return (
@@ -26,50 +18,7 @@ export default function Page() {
       </div>
 
       {/* Bottom Section */}
-      <div className="border bg-white p-4">
-        <div className="flex gap-4">
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select spoken language" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Fruits</SelectLabel>
-                <SelectItem value="apple">Apple</SelectItem>
-                <SelectItem value="banana">Banana</SelectItem>
-                <SelectItem value="blueberry">Blueberry</SelectItem>
-                <SelectItem value="grapes">Grapes</SelectItem>
-                <SelectItem value="pineapple">Pineapple</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select translation" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Fruits</SelectLabel>
-                <SelectItem value="apple">Apple</SelectItem>
-                <SelectItem value="banana">Banana</SelectItem>
-                <SelectItem value="blueberry">Blueberry</SelectItem>
-                <SelectItem value="grapes">Grapes</SelectItem>
-                <SelectItem value="pineapple">Pineapple</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="mt-4 p-2 border rounded bg-gray-100">
-          <p className="text-sm">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
-            aperiam dolores dolorem eius! Iure doloremque porro minus earum
-            ratione suscipit nisi illum quae dolores error! Minima ea placeat
-            ipsam laborum saepe commodi aperiam nesciunt illum error nihil
-            praesentium corporis quibusdam explicabo, voluptates debitis
-            assumenda iusto sint ipsa sunt quo velit.
-          </p>
-        </div>
-      </div>
+      <SelectLanguage />
     </div>
   );
 }
