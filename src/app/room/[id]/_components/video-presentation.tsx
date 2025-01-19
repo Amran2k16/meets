e80302aaea791dd61c7aa60cd9589fd7ad8e5364
amran <amranahmed234@gmail.com>
2025-01-20 02:07:54 +0400

    Need to find a way to group all the mediastreams that belong to the same socketId so we can render and control them all as part of one media stream instead of separate ones

--- src/app/room/[id]/_components/video-presentation.tsx
+++ src/app/room/[id]/_components/video-presentation.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { useRef, useEffect } from "react";
 
-type Video = {
+export type Video = {
   id: number;
   title: string;
   thumbnail: string;
+  producerId: string;
+  stream: MediaStream;
 };
 
 type VideoPresentationProps = {
@@ -21,35 +24,37 @@ export default function VideoPresentation({
   presentingVideoId,
   setPresentingVideoId,
 }: VideoPresentationProps) {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    console.log("User Videos:", userVideos);
+    if (videoRef.current && presentingVideoId !== null) {
+      const currentVideo = userVideos.find((video) => video.id === presentingVideoId);
+      if (currentVideo) {
+        videoRef.current.srcObject = currentVideo.stream;
+        videoRef.current.play();
+      }
+    }
+  }, [presentingVideoId, userVideos]);
+
   return (
     <div className={cn("flex-1 flex flex-col gap-4 pb-4")}>
       {isPresentationMode ? (
         <div className="flex flex-col gap-4">
           <div className="w-full h-[50vh] bg-muted/50 rounded-xl flex items-center justify-center">
-            <span className="text-center text-muted-foreground text-2xl">
-              {
-                userVideos.find((video) => video.id === presentingVideoId)
-                  ?.title
-              }
-            </span>
+            <video ref={videoRef} className="w-full h-full" autoPlay playsInline controls />
           </div>
 
           <div className="flex overflow-x-auto gap-4">
             {userVideos.map((video) => (
               <div
                 key={video.id}
-                className={cn(
-                  "aspect-video w-24 md:w-32 rounded-xl cursor-pointer flex-shrink-0",
-                  video.thumbnail,
-                  {
-                    "border-4 border-primary": video.id === presentingVideoId,
-                  }
-                )}
+                className={cn("aspect-video w-24 md:w-32 rounded-xl cursor-pointer flex-shrink-0", video.thumbnail, {
+                  "border-4 border-primary": video.id === presentingVideoId,
+                })}
                 onClick={() => setPresentingVideoId(video.id)}
               >
-                <span className="block text-center text-muted-foreground text-sm">
-                  {video.title}
-                </span>
+                <span className="block text-center text-muted-foreground text-sm">{video.title}</span>
               </div>
             ))}
           </div>
@@ -57,16 +62,23 @@ export default function VideoPresentation({
       ) : (
         <div className="grid auto-rows-min gap-4 md:grid-cols-4">
           {userVideos.map((video) => (
-            <div
-              key={video.id}
-              className={cn(
-                "aspect-video rounded-xl cursor-pointer",
-                video.thumbnail
-              )}
-            >
-              <span className="block text-center text-muted-foreground">
-                {video.title}
-              </span>
+            <div key={video.id} className={cn("aspect-video rounded-xl cursor-pointer", video.thumbnail)}>
+              <video
+                ref={(el) => {
+                  if (el) {
+                    el.srcObject = video.stream;
+                    el.onloadedmetadata = () => {
+                      el.play().catch((error) => {
+                        console.error("Error attempting to play video:", error);
+                      });
+                    };
+                  }
+                }}
+                className="w-full h-full"
+                autoPlay
+                playsInline
+              />
+              <span className="block text-center text-muted-foreground">{video.title}</span>
             </div>
           ))}
         </div>
--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,56 +1,475 @@
 "use client";
 
-import { useState } from "react";
 import { PAGE_LAYOUT_HEIGHT } from "@/consts/constants";
 import SelectLanguage from "./_components/select-language";
-import VideoPresentation from "./_components/video-presentation";
+import VideoPresentation, { Video } from "./_components/video-presentation";
 
-const userVideos = [
-  { id: 1, title: "Video 1", thumbnail: "bg-muted/50" },
-  { id: 2, title: "Video 2", thumbnail: "bg-muted/50" },
-  { id: 3, title: "Video 3", thumbnail: "bg-muted/50" },
-  { id: 4, title: "Video 4", thumbnail: "bg-muted/50" },
-];
+import { Consumer, useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
+import io, { Socket } from "socket.io-client";
+import mediasoupClient, { type types as mediaSoupTypes } from "mediasoup-client";
+
+import { ConsumerData, SOCKET_EVENTS, TRANSPORT_EVENTS, TransportData, type ServerMessageData } from "shared"; // adjust path as needed
+import { cn, emitAsync, safeEmitAsync } from "@/lib/utils";
+
+const createVideoEntry = (id: number, title: string, producerId: string, stream: MediaStream): Video => ({
+  id,
+  title,
+  thumbnail: "bg-muted/50",
+  producerId: producerId,
+  stream,
+});
 
 export default function Page() {
   const [isPresentationMode, setPresentationMode] = useState(false);
-  const [presentingVideoId, setPresentingVideoId] = useState<number | null>(
-    null
-  );
+  const [presentingVideoId, setPresentingVideoId] = useState<number | null>(null);
+
+  const socketRef = useRef<Socket | null>(null);
+  const deviceRef = useRef<mediasoupClient.Device | null>(null);
+  const sendTransportRef = useRef<any>(null);
+  const recvTransportRef = useRef<any>(null);
+
+  const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+  const [remoteStreams, setRemoteStreams] = useState<Record<string, MediaStream>>({});
+
+  const SERVER_URL = "http://localhost:3001";
+  const path = usePathname();
+  const [id, setId] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [connectMessage, setConnectMessage] = useState("");
+
+  useEffect(() => {
+    const initializeSocketConnection = async () => {
+      try {
+        // Basic test to check if server is alive
+        const response = await fetch(SERVER_URL + "/");
+        const data = await response.text();
+        console.log("HTTP GET RESPONSE:", data);
+      } catch (error) {
+        console.error("Error fetching data from server:", error);
+        handleFetchError();
+      }
+
+      // Extract the ID from the dynamic route
+      const pathParts = path.split("/");
+      const extractedId = pathParts[pathParts.length - 1];
+      setId(extractedId);
+
+      // Connect to socket
+      socketRef.current = io(SERVER_URL, {
+        reconnection: true,
+        reconnectionAttempts: 5,
+        reconnectionDelay: 1000,
+      });
+
+      // ===================
+      // Set up Listeners
+      // ===================
+      if (socketRef.current) {
+        const s = socketRef.current;
+
+        // Fired upon successful connection
+        s.on(SOCKET_EVENTS.CONNECT, async () => {
+          console.log(`Socket connected with ID: ${s.id}`);
+
+          setConnected(true);
+          setConnectMessage("Connected To Server");
+          setLoading(false);
+
+          const roomId = extractedId;
+          console.log(`Joining room with ID: ${roomId}`);
+
+          if (!socketRef.current) return;
+
+          const [joinRoomError, joinRoomData] = await safeEmitAsync<{ producerList: string[]; socketList: string[] }>(
+            socketRef.current,
+            SOCKET_EVENTS.JOIN_ROOM,
+            {
+              roomId,
+            }
+          );
+
+          if (joinRoomError) {
+            console.error("Failed to join room:", joinRoomError.message);
+            return;
+          }
+
+          // 2) Get Router RTP Capabilities
+          console.log("Requesting RTP capabilities from server...");
+          const [rtpError, rtpCapabilitiesResponse] = await safeEmitAsync<mediaSoupTypes.RtpCapabilities>(
+            socketRef.current,
+            SOCKET_EVENTS.GET_ROUTER_CAPABILITIES,
+            { roomId }
+          );
+
+          if (rtpError) {
+            console.error("Failed to retrieve RTP capabilities:", rtpError);
+            return;
+          }
+
+          console.log("RTP Capabilities received from server:", rtpCapabilitiesResponse);
+
+          // 3) Create Mediasoup Device
+          deviceRef.current = new mediasoupClient.Device();
+          await deviceRef.current.load({ routerRtpCapabilities: rtpCapabilitiesResponse! });
+          console.log("Mediasoup Device loaded");
+
+          console.log("Requesting to create transport for sending media...");
+
+          //CREATE SEND TRANSPORT
+          const [createSendTransportError, createSendTransportResponse] = await safeEmitAsync<TransportData>(
+            socketRef.current,
+            SOCKET_EVENTS.CREATE_TRANSPORT,
+            { roomId, direction: "send" }
+          );
+
+          console.log("Create Send Transport Response:", createSendTransportResponse);
+
+          if (createSendTransportError) {
+            console.error("Failed to create transport:", createSendTransportError.message);
+            return;
+          }
+
+          console.log("Transport Parameters:", JSON.stringify(createSendTransportResponse?.id));
+
+          sendTransportRef.current = deviceRef.current.createSendTransport(createSendTransportResponse!);
+
+          sendTransportRef.current.on(
+            "connect",
+            async ({ dtlsParameters }: any, callback: () => void, errback: (error: Error) => void) => {
+              console.log("SendTransport connecting...");
+              const [connectError, connectResponse] = await safeEmitAsync<any>(
+                socketRef.current!,
+                SOCKET_EVENTS.CONNECT_TRANSPORT,
+                { roomId, transportId: sendTransportRef.current.id, dtlsParameters }
+              );
+
+              if (connectError) {
+                errback(new Error(connectError.message));
+              } else {
+                callback();
+              }
+            }
+          );
+
+          sendTransportRef.current.on(
+            "produce",
+            async (
+              { kind, rtpParameters }: any,
+              callback: (arg: { id: string }) => void,
+              errback: (error: Error) => void
+            ) => {
+              if (!socketRef.current) return;
+
+              const [produceError, produceResponse] = await safeEmitAsync<any>(
+                socketRef.current,
+                SOCKET_EVENTS.PRODUCE,
+                {
+                  roomId,
+                  transportId: sendTransportRef.current.id,
+                  kind,
+                  rtpParameters,
+                }
+              );
+
+              if (produceError) {
+                errback(new Error(produceError.message));
+              } else {
+                callback({ id: produceResponse.id });
+              }
+            }
+          );
+
+          sendTransportRef.current.on(TRANSPORT_EVENTS.CONNECTION_STATE_CHANGE, (state: string) => {
+            console.log(`Send transport state changed: ${state}`);
+          });
+
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+          setLocalStream(stream);
+
+          stream.getTracks().forEach(async (track) => {
+            await sendTransportRef.current.produce({ track });
+          });
+
+          //
+          //  CREATE RECEIVE TRANSPORT
+          //
+          const [createRecvTransportError, createRecvTransportResponse] = await safeEmitAsync<TransportData>(
+            socketRef.current,
+            SOCKET_EVENTS.CREATE_TRANSPORT,
+            { roomId, direction: "recv" }
+          );
+
+          if (createRecvTransportError) {
+            throw new Error(createRecvTransportError.message);
+          }
+
+          recvTransportRef.current = deviceRef.current.createRecvTransport(createRecvTransportResponse!);
+
+          recvTransportRef.current.on(
+            "connect",
+            async ({ dtlsParameters }: any, callback: () => void, errback: (error: Error) => void) => {
+              console.log("RecvTransport connecting...");
+
+              const [connectTransportError, connectTransportResponse] = await safeEmitAsync<any>(
+                socketRef.current!,
+                SOCKET_EVENTS.CONNECT_TRANSPORT,
+                { roomId, transportId: recvTransportRef.current.id, dtlsParameters }
+              );
+
+              if (connectTransportError) {
+                errback(new Error(connectTransportResponse.error));
+              } else {
+                callback();
+              }
+            }
+          );
+
+          recvTransportRef.current.on(TRANSPORT_EVENTS.CONNECTION_STATE_CHANGE, (state: string) => {
+            console.log(`Recv transport state changed: ${state}`);
+          });
+
+          console.log("Join Room Response -> List of producers:", JSON.stringify(joinRoomData));
+
+          for (const producerId of joinRoomData?.producerList!) {
+            if (producerId === sendTransportRef.current.id) {
+              console.log(`Skipping own producer with ID: ${producerId}`);
+              continue;
+            }
+
+            const [consumeError, consumeResult] = await safeEmitAsync<ConsumerData>(
+              socketRef.current,
+              SOCKET_EVENTS.CONSUME,
+              {
+                roomId,
+                producerId,
+                transportId: recvTransportRef.current.id,
+              }
+            );
+
+            if (consumeError) {
+              console.error("Failed to join room:", consumeError.message);
+              return;
+            }
+
+            // Now create a consumer for that existing producer
+            const consumer = await recvTransportRef.current.consume(consumeResult);
+            const stream = new MediaStream([consumer.track]);
+            setRemoteStreams((prev) => ({ ...prev, [producerId]: stream }));
+
+            consumer.on("trackended", () => {
+              console.log(`Remote track ended for producer: ${producerId}`);
+              setRemoteStreams((prev) => {
+                const updated = { ...prev };
+                delete updated[producerId];
+                return updated;
+              });
+            });
+          }
+        });
+
+        s.on(SOCKET_EVENTS.CONNECT_ERROR, handleConnectError);
+        s.on(SOCKET_EVENTS.DISCONNECT, handleDisconnect);
+        s.on(SOCKET_EVENTS.SERVER_MESSAGE, handleServerMessage);
+        s.on(SOCKET_EVENTS.USER_LEFT, handleUserLeft);
+        // Fired when server notifies about a new producer
+        s.on(SOCKET_EVENTS.NEW_PRODUCER, handleNewProducer);
+      }
+
+      return () => {
+        if (socketRef.current) {
+          const s = socketRef.current;
+          s.off(SOCKET_EVENTS.CONNECT, handleConnect);
+          s.off(SOCKET_EVENTS.CONNECT_ERROR, handleConnectError);
+          s.off(SOCKET_EVENTS.DISCONNECT, handleDisconnect);
+          s.off(SOCKET_EVENTS.SERVER_MESSAGE, handleServerMessage);
+          s.off(SOCKET_EVENTS.NEW_PRODUCER, handleNewProducer);
+
+          // Disconnect
+          s.disconnect();
+          console.log("Disconnected from the WebSocket server");
+        }
+      };
+    };
+
+    initializeSocketConnection();
+  }, [path]);
+
+  // ================================================
+  // Handler Functions for Socket Connection
+  // ================================================
+  const handleFetchError = () => {
+    setConnected(false);
+    setConnectMessage("Failed to get response from HTTP Server");
+    setLoading(false);
+  };
+
+  const handleConnectError = () => {
+    console.error("Connection to WebSocket server failed");
+    setConnected(false);
+    setConnectMessage("Failed To Connect To Server");
+    setLoading(false);
+  };
+
+  const handleConnect = () => {};
+
+  const handleDisconnect = (reason: string) => {
+    console.log(`Disconnected from the WebSocket server. Reason: ${reason}`);
+    setConnected(false);
+    setConnectMessage("Disconnected from server");
+  };
+
+  const handleServerMessage = (data: ServerMessageData) => {
+    console.log("Received server message:", data.message);
+  };
+
+  const handleUserLeft = ({
+    message,
+    socketId,
+    producerIds,
+  }: {
+    message: string;
+    socketId: string;
+    producerIds: string[];
+  }) => {
+    console.log("Handling User Left :", message);
+    setRemoteStreams((prev) => {
+      const updatedStreams = { ...prev };
+      producerIds.forEach((id) => {
+        if (updatedStreams[id]) {
+          delete updatedStreams[id];
+        }
+      });
+      return updatedStreams;
+    });
+  };
+
+  // ================================================
+  // Handle a new producer (i.e., someone else joined)
+  // ================================================
+  const handleNewProducer = async ({ socketId, producerId }: { socketId: string; producerId: string }) => {
+    console.log(`Handling new producer from server: ${producerId}`);
+
+    console.log("Sending consume request to server with:", {
+      roomId: path.split("/").pop() || "",
+      producerId,
+      transportId: recvTransportRef.current.id,
+    });
+
+    const [consumeError, consumerParameters] = await safeEmitAsync<mediaSoupTypes.Consumer>(
+      socketRef.current!,
+      SOCKET_EVENTS.CONSUME,
+      {
+        roomId: path.split("/").pop() || "", // dynamically extract the latest id from the path
+        producerId,
+        transportId: recvTransportRef.current.id,
+      }
+    );
+
+    if (consumeError) {
+      console.error("Error consuming producer:", consumeError.message);
+      return;
+    }
+
+    console.log("Consumer parameters:", JSON.stringify(consumerParameters));
+
+    // Create a new consumer
+    const consumer = await recvTransportRef.current.consume(consumerParameters);
+    console.log(`Consumer created for producer: ${producerId}`);
+
+    // Attach the consumer track to a new MediaStream
+    const stream = new MediaStream([consumer.track]);
+    console.log(`Stream ID: ${stream.id}, Producer ID: ${producerId}`);
+    setRemoteStreams((prev) => ({ ...prev, [producerId]: stream }));
+
+    // Clean up on track end
+    consumer.on("trackended", () => {
+      console.log(`Remote track ended for producer: ${producerId}`);
+      setRemoteStreams((prev) => {
+        const updated = { ...prev };
+        delete updated[producerId];
+        return updated;
+      });
+    });
+  };
 
   const togglePresentationMode = () => {
     setPresentationMode((prevMode) => !prevMode);
     if (!isPresentationMode) {
-      setPresentingVideoId(userVideos[0]?.id || null);
+      setPresentingVideoId(videos[0]?.id || null);
     } else {
       setPresentingVideoId(null);
     }
   };
 
   return (
-    <div
-      className="flex flex-col px-2 md:px-4 pt-2 md:pt-4"
-      style={{ minHeight: PAGE_LAYOUT_HEIGHT }}
-    >
+    <div className="flex flex-col px-2 md:px-4 pt-2 md:pt-4" style={{ minHeight: PAGE_LAYOUT_HEIGHT }}>
       {/* Toggle Button */}
       <div className="mb-4 flex justify-end">
-        <button
-          onClick={togglePresentationMode}
-          className="px-4 py-2 bg-primary text-white rounded-lg shadow"
-        >
-          {isPresentationMode
-            ? "Exit Presentation Mode"
-            : "Enter Presentation Mode"}
+        <button onClick={togglePresentationMode} className="px-4 py-2 bg-primary text-white rounded-lg shadow">
+          {isPresentationMode ? "Exit Presentation Mode" : "Enter Presentation Mode"}
         </button>
       </div>
 
       {/* Main Content */}
-      <VideoPresentation
-        userVideos={userVideos}
+      <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+        {localStream && (
+          <div className={cn("aspect-video rounded-xl cursor-pointer", "bg-muted/50")}>
+            <video
+              ref={(el) => {
+                if (el) {
+                  el.srcObject = localStream;
+                  el.onloadedmetadata = () => {
+                    el.play().catch((error) => {
+                      console.error("Error attempting to play local video:", error);
+                    });
+                  };
+                }
+              }}
+              className="w-full h-full"
+              autoPlay
+              playsInline
+              muted
+            />
+            <span className="block text-center text-muted-foreground">Local Stream</span>
+          </div>
+        )}
+
+        {Object.entries(remoteStreams).map(([producerId, stream]) => (
+          <div key={producerId} className={cn("aspect-video rounded-xl cursor-pointer", "bg-muted/50")}>
+            <video
+              ref={(el) => {
+                if (el) {
+                  el.srcObject = stream;
+                  el.onloadedmetadata = () => {
+                    el.play().catch((error) => {
+                      console.error("Error attempting to play remote video:", error);
+                    });
+                  };
+                }
+              }}
+              className="w-full h-full"
+              autoPlay
+              playsInline
+              controls
+            />
+            <span className="block text-center text-muted-foreground">
+              Remote Stream {producerId} :
+              {stream
+                .getTracks()
+                .map((track) => track.kind)
+                .join(", ")}
+            </span>
+          </div>
+        ))}
+      </div>
+      {/* <VideoPresentation
+        userVideos={videos}
         isPresentationMode={isPresentationMode}
         presentingVideoId={presentingVideoId}
         setPresentingVideoId={setPresentingVideoId}
-      />
+      /> */}
 
       {/* Bottom Section */}
       <SelectLanguage />
